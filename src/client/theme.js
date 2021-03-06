0a9cb612a75a9b9be40ddcdd606d2926ff15f66a
Drylozu <35281350+Drylozu@users.noreply.github.com>
2021-03-06 15:54:54 -0500

    a

--- src/client/theme.js
+++ src/client/theme.js
@@ -3,7 +3,5 @@ import styles from './styles/index.css';
 if (!localStorage.getItem('darkMode'))
     localStorage.setItem('darkMode', window.matchMedia('(prefers-color-scheme: dark)').matches);
 
-const config = JSON.parse(localStorage.getItem('darkMode'));
-
-if (config)
+if (JSON.parse(localStorage.getItem('darkMode')))
     document.body.classList.add(styles.darkTheme);
