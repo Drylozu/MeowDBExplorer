4b50c67c9de8192903082e87a3eaccd4afa0eeff
Drylozu <35281350+Drylozu@users.noreply.github.com>
2021-09-08 08:44:07 -0500

    add generic types to useLocalStorage

--- hooks/useLocalStorage.tsx
+++ hooks/useLocalStorage.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 
-export default function useLocalStorage(key: string, initial: any) {
-    const [value, setValue] = useState(() => {
+export default function useLocalStorage<T extends any>(key: string, initial: T): [T, (value: T) => void, () => void] {
+    const [value, setValue] = useState<T>(() => {
         if (typeof window !== 'undefined') {
             const data = window.localStorage.getItem(key);
-            if (data !== null) return JSON.parse(data);
+            if (data !== null)
+                try {
+                    return JSON.parse(data);
+                } catch { }
         }
         return initial;
     });
